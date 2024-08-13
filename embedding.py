ec91c8dbc726aab6fbeb0d1a9ce9231cee2e60ca
marvin <sea-son@foxmail.com>
2024-08-14 06:42:58 +0800

    fix: format

--- embedding.py
+++ embedding.py
@@ -1,47 +1,48 @@
 import chromadb
 import ollama
 
+embedding_model = 'mxbai-embed-large'
 documents = [
-  "Llamas are members of the camelid family meaning they're pretty closely related to vicuñas and camels",
-  "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands",
-  "Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall",
-  "Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight",
-  "Llamas are vegetarians and have very efficient digestive systems",
-  "Llamas live to be about 20 years old, though some only live for 15 years and others live to be 30 years old",
+    "Llamas are members of the camelid family meaning they're pretty closely related to vicuñas and camels",
+    "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands",
+    "Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall",
+    "Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight",
+    "Llamas are vegetarians and have very efficient digestive systems",
+    "Llamas live to be about 20 years old, though some only live for 15 years and others live to be 30 years old",
 ]
 
 dbclient = chromadb.Client()
 collection = dbclient.create_collection(name="docs")
 
-ollama = ollama.Client(host="http://localhost:11434");
+ollama = ollama.Client(host="http://localhost:11434")
 
 # store each document in a vector embedding database
 for i, d in enumerate(documents):
-  response = ollama.embeddings(
-    model='chatfire/bge-m3:q8_0', 
-    prompt='Llamas are members of the camelid family', 
-  )
-  embedding = response["embedding"]
-  collection.add(ids=[str(i)], embeddings=[embedding], documents=[d])
+    response = ollama.embeddings(
+        model=embedding_model,
+        prompt='Llamas are members of the camelid family',
+    )
+    embedding = response["embedding"]
+    collection.add(ids=[str(i)], embeddings=[embedding], documents=[d])
 
 # an example prompt
 prompt = "What animals are llamas related to?"
 
 # generate an embedding for the prompt and retrieve the most relevant doc
 response = ollama.embeddings(
-  prompt=prompt,
-  model="chatfire/bge-m3:q8_0"
+    prompt=prompt,
+    model="mxbai-embed-large"
 )
 results = collection.query(
-  query_embeddings=[response["embedding"]],
-  n_results=1
+    query_embeddings=[response["embedding"]],
+    n_results=1
 )
 data = results['documents'][0][0]
 
 # generate a response combining the prompt and data we retrieved in step 2
 output = ollama.generate(
-  model="llama3.1",
-  prompt=f"Using this data: {data}. Respond to this prompt: {prompt}"
+    model="llama3.1",
+    prompt=f"Using this data: {data}. Respond to this prompt: {prompt}"
 )
 
 print(output['response'])
